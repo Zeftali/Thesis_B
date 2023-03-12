c1eb37cc1c9be049bc519631fa1994374cf2b8ab
Zeftali <zoheb.eftali@gmail.com>
2023-03-13 01:49:00 +1100

    Edited README document to better explain current code as well as future modifications

--- neural_network.py
+++ neural_network.py
@@ -1,13 +1,30 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM
+from keras.layers import Dense, LSTM, Dropout
+from keras.callbacks import EarlyStopping
 
 # define the model architecture
 model = Sequential()
-model.add(LSTM(units=50, input_shape=(timesteps, input_dim)))
-model.add(Dense(units=output_dim, activation='softmax'))
+
+# add LSTM layer with dropout
+model.add(LSTM(units=64, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+
+# add another LSTM layer with dropout
+model.add(LSTM(units=64, return_sequences=True))
+model.add(Dropout(0.2))
+
+# add third LSTM layer with dropout
+model.add(LSTM(units=64))
+model.add(Dropout(0.2))
+
+# add output layer
+model.add(Dense(units=1))
 
 # compile the model
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# set early stopping to prevent overfitting
+early_stop = EarlyStopping(monitor='val_loss', patience=3)
 
 # train the model
-model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_data=(X_val, Y_val))
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stop])
