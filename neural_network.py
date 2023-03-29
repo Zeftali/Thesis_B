8f92abd529f27f74206f775c48f5929c8ffb1efb
Zeftali <zoheb.eftali@gmail.com>
2023-03-29 18:10:16 +1100

    Activation, Normalisation, Regularisation layers implemented with respective functions

--- neural_network.py
+++ neural_network.py
@@ -1,21 +1,33 @@
 from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
-from keras.callbacks import EarlyStopping
+from keras.layers import Dense, LSTM, Dropout, Activation, BatchNormalization
+from keras.regularizers import l2
 
 # define the model architecture
 model = Sequential()
 
-# add LSTM layer with dropout
-model.add(LSTM(units=64, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True))
+# add LSTM layer with dropout, activation, normalization and regularisation
+model.add(LSTM(units=64, input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=True, kernel_regularizer=l2(0.01)))
 model.add(Dropout(0.2))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
 
-# add another LSTM layer with dropout
-model.add(LSTM(units=64, return_sequences=True))
+# add another LSTM layer with dropout, activation, normalization and regularisation
+model.add(LSTM(units=64, return_sequences=True, kernel_regularizer=l2(0.01)))
 model.add(Dropout(0.2))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
 
-# add third LSTM layer with dropout
-model.add(LSTM(units=64))
+# add third LSTM layer with dropout, activation, normalization and regularisation
+model.add(LSTM(units=64, kernel_regularizer=l2(0.01)))
 model.add(Dropout(0.2))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
+
+#add fourth LSTM layer with dropout, activation, normalisation and regularisation 
+model.add(LSTM(units=64, kernel_regularizer=l2(0.01)))
+model.add(Dropout(0.2))
+model.add(Activation('relu'))
+model.add(BatchNormalization())
 
 # add output layer
 model.add(Dense(units=1))
@@ -24,7 +36,7 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # set early stopping to prevent overfitting
-early_stop = EarlyStopping(monitor='val_loss', patience=3)
+early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose='1')
 
 # train the model
-history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stop])
+history = model.fit(X_train, y_train, epochs=100, batch_size=120, validation_data=(X_val, y_val), callbacks=[early_stop])
