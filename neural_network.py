3c7142c60a0941c43e56eb921f7fc6664a40e555
Zeftali <zoheb.eftali@gmail.com>
2023-04-28 02:01:17 +1000

    Slight changes

--- neural_network.py
+++ neural_network.py
@@ -5,6 +5,8 @@ from keras.layers import Dense, LSTM, Dropout, Activation, BatchNormalization
 from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from keras.optimizers import Adam
 
 def prepare_data():
     # load the data from an Excel file 
@@ -20,8 +22,8 @@ def prepare_data():
     data = data.select_dtypes(include=np.number)
 
     # split data into X and y
-    target_col = 'Flow_Duration'
-    X = data.drop(columns=[target_col], axis=1).values
+    target_col = 'Bwd_Blk_Rate_Avg'
+    X = data.drop(columns=[target_col], axis=0).values
     y = data[target_col].values
 
     # PCA for feature extraction 
@@ -29,13 +31,11 @@ def prepare_data():
     X = pca.fit_transform(X)
 
     # split the data into training and validation sets 
-    train_size = int(0.8 * len(X))
-    X_train, y_train = X[:train_size], y[:train_size]
-    X_val, y_val = X[train_size:], y[train_size:]
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # get number of input features and output classes 
     num_input_features = X_train.shape[1]
-    num_classes = 970
+    num_classes = 831
 
     return X_train.reshape(-1, 1, num_input_features), y_train, X_val.reshape(-1, 1, num_input_features), y_val, num_input_features, num_classes
 
@@ -44,19 +44,25 @@ def define_model(num_input_features, num_classes):
     model = Sequential()
 
     # add LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=64, input_shape = (1, num_input_features), return_sequences=True, kernel_regularizer=l2(0.01)))
+    model.add(LSTM(units=128, input_shape = (1, num_input_features), return_sequences=True, kernel_regularizer=l2(0.01)))
     model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add another LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=64, return_sequences=True, kernel_regularizer=l2(0.01)))
+    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.01)))
     model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add third LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=64, kernel_regularizer=l2(0.01)))
+    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.01)))
+    model.add(Dropout(0.2))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+
+    # add third LSTM layer with dropout, activation, normalization and regularisation
+    model.add(LSTM(units=128, kernel_regularizer=l2(0.01)))
     model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
@@ -65,7 +71,7 @@ def define_model(num_input_features, num_classes):
     model.add(Dense(units=num_classes, activation='softmax'))
 
     # compile the model
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     return model
 
@@ -79,15 +85,10 @@ def train_model():
     early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
     # train the model
-    model.fit(X_train, y_train, batch_size=64, epochs=10, validation_data=(X_val, y_val), callbacks=[early_stop])
+    model.fit(X_train, y_train, batch_size=128, epochs=100, validation_data=(X_val, y_val), callbacks=[early_stop])
     return model
 
 
 if __name__ == '__main__':
     # train the model and get the trained model
     model = train_model()
-
-    # evaluate the model on validation data and print accuracy
-    X_train, y_train, X_val, y_val, num_input_features, num_classes = prepare_data()
-    score = model.evaluate(X_val.reshape(-1, 1, num_input_features), y_val, verbose=0)
-    print('Validation accuracy:', score[1])
