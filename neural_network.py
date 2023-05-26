94e34f7810883cf1ca36ac78e09e2275d8883b8d
Zeftali <zoheb.eftali@gmail.com>
2023-05-27 01:42:39 +1000

    Few changes

--- neural_network.py
+++ neural_network.py
@@ -8,8 +8,7 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
-
-
+from sklearn.preprocessing import StandardScaler
 
 def prepare_data():
     # load the data from an Excel file 
@@ -25,12 +24,16 @@ def prepare_data():
     data = data.select_dtypes(include=np.number)
 
     # split data into X and y
-    target_cols = ['Tot_Fwd_Pkts', 'Tot_Bwd_Pkts', 'Flow_Pkts/s', 'Flow_Byts/s', 'Flow_Duration']
+    target_cols = ['Tot_Fwd_Pkts', 'Tot_Bwd_Pkts', 'Flow_Pkts/s', 'Flow_Byts/s']
     X = data.drop(columns=['Idle_Mean', 'Idle_Max', 'Idle_Min', 'Init_Bwd_Win_Byts', 'Subflow_Bwd_Pkts'], axis=1).values
     y = data[target_cols].values
 
-    # PCA for feature extraction 
-    pca = PCA(n_components=1)
+    #normalise the input features
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    # PCA for feature extraction. Determined number of components using explained variance ratio.
+    pca = PCA(n_components=4)
     X = pca.fit_transform(X)
 
     # split the data into training and validation sets 
@@ -47,31 +50,31 @@ def define_model(num_input_features, num_classes):
     model = Sequential()
 
     # add LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=128, input_shape = (1, num_input_features), return_sequences=True, kernel_regularizer=l2(0.01)))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, input_shape = (1, num_input_features), return_sequences=True, kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add another LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.01)))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add third LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.01)))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add fourth LSTM layer with dropout, activation, normalization and regularisation
-    model.add(LSTM(units=128, kernel_regularizer=l2(0.01)))
-    model.add(Dropout(0.2))
+    model.add(LSTM(units=128, kernel_regularizer=l2(0.001)))
+    model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
     # add output layer
-    model.add(Dense(units=num_classes, activation='softmax'))
+    model.add(Dense(units=num_classes))
 
     # compile the model
     model.compile(optimizer=Adam(learning_rate=0.0001), loss='mean_squared_logarithmic_error', metrics=['accuracy'])
@@ -88,7 +91,7 @@ def train_model():
     early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
 
     # train the model
-    history = model.fit(X_train, y_train, batch_size=128, epochs=100, validation_data=(X_val, y_val), callbacks=[early_stop])
+    history = model.fit(X_train, y_train, batch_size=128, epochs=300, validation_data=(X_val, y_val), callbacks=[early_stop])
    
    # plot the training and validation metrics
     plt.plot(history.history['accuracy'])
