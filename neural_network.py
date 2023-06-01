73067a59fce616d8e17ab77d28d4f3557ddb95b9
Zeftali <zoheb.eftali@gmail.com>
2023-06-01 18:51:01 +1000

    Changes

--- neural_network.py
+++ neural_network.py
@@ -14,12 +14,12 @@ from sklearn.utils import shuffle
 from imblearn.over_sampling import RandomOverSampler
 
 def prepare_data():
-    # load the data from an Excel file
+    # load the data from an Excel file 
     data = pd.read_csv('IoT-DS2.csv')
-
+    
     # drop null values
     data.dropna(inplace=True)
-
+    
     # encode categorical variables
     data = pd.get_dummies(data)
 
@@ -27,6 +27,12 @@ def prepare_data():
     target_col = 'Cat'
     X = data.drop(columns=[target_col], axis=1).values
     y = data[target_col].map({'Normal': 0, 'Mirai': 1}).values
+
+    # num of classes for binary classification
+    num_classes = len(np.unique(y)) 
+
+    # convert y to numpy array and reshape for compatibility with the model
+    y = np.array(y).reshape(-1, 1)
 
     # normalize the input features
     scaler = StandardScaler()
@@ -37,16 +43,16 @@ def prepare_data():
     X = pca.fit_transform(X)
 
     # Shuffle the data
-    X, y_binary = shuffle(X, y_binary, random_state=None)
+    X, y = shuffle(X, y, random_state=None)
 
     # Apply Random Over-Sampling
     oversampler = RandomOverSampler()
-    X, y_binary = oversampler.fit_resample(X, y_binary)
+    X, y = oversampler.fit_resample(X, y)
 
     # split the data into training and validation sets
-    X_train, X_val, y_train, y_val = train_test_split(X, y_binary, test_size=0.2, random_state=None)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=None)
 
-    # get the number of input features
+    # get the number of input features and output classes
     num_input_features = X_train.shape[1]
 
     return (
@@ -55,34 +61,35 @@ def prepare_data():
         X_val.reshape(-1, 1, num_input_features),
         y_val,
         num_input_features,
+        num_classes,
     )
 
 # define the model architecture
-def define_model(num_input_features):
+def define_model(num_input_features, num_classes):
     model = Sequential()
-    # add LSTM layer with dropout, activation, normalization, and regularization
+    # add LSTM layer with dropout, activation, normalization and regularisation
     model.add(LSTM(units=128, input_shape=(1, num_input_features), return_sequences=True, kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
-    # add another LSTM layer with dropout, activation, normalization, and regularization
+    # add another LSTM layer with dropout, activation, normalization and regularisation
     model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
-    # add third LSTM layer with dropout, activation, normalization, and regularization
+    # add third LSTM layer with dropout, activation, normalization and regularisation
     model.add(LSTM(units=128, return_sequences=True, kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
-    # add fourth LSTM layer with dropout, activation, normalization, and regularization
+    # add fourth LSTM layer with dropout, activation, normalization and regularisation
     model.add(LSTM(units=128, kernel_regularizer=l2(0.001)))
     model.add(Dropout(0.3))
     model.add(Activation('relu'))
     model.add(BatchNormalization())
 
-    # add output layer with sigmoid activation for binary classification
-    model.add(Dense(units=1, activation="sigmoid"))
+    # add output layer
+    model.add(Dense(units=num_classes, activation='sigmoid'))
 
     # compile the model
     model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
@@ -90,15 +97,15 @@ def define_model(num_input_features):
     return model
 
 def train_model():
-    X_train, y_train, X_val, y_val, num_input_features = prepare_data()
+    X_train, y_train, X_val, y_val, num_input_features, num_classes = prepare_data()
     # define the model
-    model = define_model(num_input_features)
+    model = define_model(num_input_features, num_classes)
     # set early stopping to prevent overfitting
     early_stop = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
     # train the model
     history = model.fit(X_train, y_train, batch_size=128, epochs=100, validation_data=(X_val, y_val), callbacks=[early_stop])
-
-    # plot the training and validation metrics
+   
+   # plot the training and validation metrics
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
     plt.title('Model Accuracy')
@@ -116,7 +123,10 @@ def train_model():
 
     # evaluate the model on the validation set
     y_val_pred_prob = model.predict(X_val)
-    y_val_pred = np.round(y_val_pred_prob).flatten()
+    y_val_pred = (y_val_pred_prob > 0.5).astype(int)
+
+    # convert y_val to numpy array and reshape for compatibility with classification metrics
+    y_val = np.array(y_val).reshape(-1)
 
     # compute classification metrics
     precision = precision_score(y_val, y_val_pred)
@@ -126,7 +136,7 @@ def train_model():
     print("Precision: {:.4f}".format(precision))
     print("Recall: {:.4f}".format(recall))
     print("F1-score: {:.4f}".format(f1))
-
+    
     # generate classification report and confusion matrix
     report = classification_report(y_val, y_val_pred)
     matrix = confusion_matrix(y_val, y_val_pred)
